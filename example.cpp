faf40b72a9b219dacd69c4f780b69f3ed9cefc20
U-BSD\453480 <453480@SHS-GYBF6X3.beaverton.k12.or.us>
2025-09-05 10:16:39 -0700

    New Save

--- example.cpp
+++ example.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-  cout << "Hello";
+void randomnumber(){
+  //cout << rand();
+
+  int RandomNumber;
+  cout << ("Enter your random number for now: ");
+  cin >> RandomNumber;
+
+  cout << "\n";
+}
+
+void numberguess(){
+  int NumberGuess;
+  cout << ("Your number?: ");
+  cin >> NumberGuess;
 
 }
 
+int main(){
+  
+  cout << "Guess a number and I will tell you if its too high or low.";
+  cout << "\n";
+
+  randomnumber();
+  numberguess();
+
+  return 0;
+
+}
