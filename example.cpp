8a54e374a87a556b8c7c15f770ce963b78dc9691
U-BSD\453480 <453480@SHS-GYBF6X3.beaverton.k12.or.us>
2025-09-09 10:19:23 -0700

    save

--- example.cpp
+++ example.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
-void randomnumber(){
-  //cout << rand();
-
-  int RandomNumber;
-  cout << ("Enter your random number for now: ");
-  cin >> RandomNumber;
-
-  cout << "\n";
-}
-
-void numberguess(){
-  int NumberGuess;
-  cout << ("Your number?: ");
-  cin >> NumberGuess;
-
-}
-
-void numberlogic(){
-  if (NumberGuess > RandomNumber);
-}
-
-int main(){
+int RandomNumber() {
+  int randomnumber;
+  randomnumber = srand();
   
+  return randomnumber;
+}
+
+int NumberGuess() {
+  int numberguess;
+  cout << "Your number?: ";
+  cin >> numberguess;
+  return numberguess;
+}
+
+
+int main() {
+
   cout << "Guess a number and I will tell you if its too high or low.";
   cout << "\n";
 
-  randomnumber();
-  numberguess();
+  int randomnumber = RandomNumber();  
+  int numberguess = NumberGuess();
+  
+  if (numberguess < randomnumber) {
+    cout << "Wrong answer! The random number " << numberguess << ", is low." << endl;
+  }
+  else if (numberguess > randomnumber) {
+    cout << "Wrong answer! The random number " << numberguess << ", is high." << endl; 
+  }
+  else if (numberguess = randomnumber) {
+    cout << "You guessed it!" << endl;
+  }
 
   return 0;
-
 }
