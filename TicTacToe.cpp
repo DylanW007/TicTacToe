683dcf0c0bc2c270a9be47d83b2b7442ec20651f
U-BSD\453480 <453480@SHS-GYBF6X3.beaverton.k12.or.us>
2025-10-06 09:56:30 -0700

    save

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main(){
-  cout << "Hello" << endl;
 
+  char board[3][3] = {{'.','.','.'},{'.','.','.'},{'.','.','.'}};
+
+  int i, t;
+  
+  cout << "Printing Board: \n";
+  for (i = 0; i < 3; i++){
+    for (t = 0; t < 3; t++){
+      cout << "\t" << board[i][t];
+    }
+  }
+  
   return 0;
 }
